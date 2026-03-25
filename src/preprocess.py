2728cf125caea72e1a7c6cc807801d93e4012264
Dhvani Yash Chaudhari <dhvanichaudhari20@gmail.com>
2026-03-25 15:35:27 -0500

    Updated preprocess.py to return scaler for Gradio app setup

--- src/preprocess.py
+++ src/preprocess.py
@@ -28,4 +28,4 @@ def preprocess_data(df):
     smote = SMOTE(random_state=42)
     X_train, y_train = smote.fit_resample(X_train, y_train)
 
-    return X_train, X_test, y_train, y_test
+    return X_train, X_test, y_train, y_test, scaler
